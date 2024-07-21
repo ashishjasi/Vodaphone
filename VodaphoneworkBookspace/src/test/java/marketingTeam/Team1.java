6b1862557790badf3c96ba5741564e035d8f6845
Nilamber <nilambersingh@gmail.com>
2024-07-21 09:36:14 +0530

    add

---
+++ VodaphoneworkBookspace/src/test/java/marketingTeam/Team1.java
@@ -0,0 +1,10 @@
+package marketingTeam;
+
+public class Team1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Marketing team get successfully created ");
+	}
+
+}
---
+++ VodaphoneworkBookspace/src/test/java/salesemployee/Team2.java
@@ -0,0 +1,10 @@
+package salesemployee;
+
+public class Team2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Sales team successfully created");
+	}
+
+}
