f766c533bd436b6c73622b2c3d7996354d9a9cda
arjune221105 <shakti221105@gmail.com>
2024-07-20 21:19:30 -0700

    add

---
+++ VodaphoneworkBookspace/src/test/java/Employee_001/ToCreateEmployee_001.java
@@ -0,0 +1,12 @@
+package Employee_001;
+
+public class ToCreateEmployee_001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Every  Day working 4K Employee");
+		System.out.println("Successfully");
+
+	}
+
+}
